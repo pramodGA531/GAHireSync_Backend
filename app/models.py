923e2baa81fb8b9f5956afdef97b2bf01593d1a1
Satya2553 <mareedu.satyanarayana.5475@gmail.com>
2025-01-02 10:51:56 +0530

    Setup

--- app/models.py
+++ app/models.py
@@ -60,7 +60,7 @@ class Organization(models.Model):
     company_address = models.CharField(max_length=255,unique=True)
     is_subscribed = models.BooleanField(default=False)
     manager = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="managing_organization")
-    recruiters = models.ManyToManyField(CustomUser, related_name="recruiting_organization", blank=True)
+    recruiters = models.ManyToManyField(CustomUser, related_name="recruiting_organization", blank=True,null=True)
 
     def __str__(self):
         return self.name
