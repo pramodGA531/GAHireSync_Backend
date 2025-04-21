fb24e8e582fcd61a826b12f334f601b24ff86d4f
sivakalki <sivakalkipusarla6@gmail.com>
2025-04-21 11:40:31 +0530

    updated field in notifications

--- migrate.sh
+++ migrate.sh
@@ -1,9 +1,3 @@
-export DJANGO_SUPERUSER_USERNAME=admin && \
-export DJANGO_SUPERUSER_EMAIL=admin@example.com && \
-export DJANGO_SUPERUSER_PASSWORD=your_secure_password && \
 python manage.py makemigrations app && \
 python manage.py migrate && \
-python manage.py createsuperuser --noinput \
-  --username "$DJANGO_SUPERUSER_USERNAME" \
-  --email "$DJANGO_SUPERUSER_EMAIL" && \
 gunicorn RTMAS_BACKEND.wsgi:application
