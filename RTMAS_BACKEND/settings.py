8ff99f823864ca5d44f9a19c3096a097adc12e9b
sivakalki <sivakalkipusarla6@gmail.com>
2025-07-03 11:46:42 +0530

    completed docker setup

--- RTMAS_BACKEND/settings.py
+++ RTMAS_BACKEND/settings.py
@@ -206,11 +206,14 @@ GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
 
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  # RabbitMQ default URL
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'  # RabbitMQ default URL
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_TIMEZONE = 'Asia/Kolkata'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+
 
 
 CELERY_BEAT_SCHEDULE = {
