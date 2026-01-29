64f392771b1498dbb0a238962bbcb055ec1fb6cc
Sai Venkata Sirisha Motupalli <motupallisnmsdurgapramod531@gmail.com>
2026-01-29 15:42:15 +0530

    backend url changed

--- migrate.sh
+++ migrate.sh
@@ -6,3 +6,4 @@ python manage.py createsuperuser --noinput \
   --username "$DJANGO_SUPERUSER_USERNAME" \
   --email "$DJANGO_SUPERUSER_EMAIL" && \
 gunicorn RTMAS_BACKEND.wsgi:application
+# gunicorn RTMAS_BACKEND.wsgi:application --bind 0.0.0.0:$PORT
