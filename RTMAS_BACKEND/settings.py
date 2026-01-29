29509777fa88dbb494eca4f0e1fe787ff5f441c7
Sai Venkata Sirisha Motupalli <motupallisnmsdurgapramod531@gmail.com>
2026-01-29 11:31:16 +0530

    requirements updated

--- RTMAS_BACKEND/settings.py
+++ RTMAS_BACKEND/settings.py
@@ -100,7 +100,7 @@ if ENVIRONMENT == "localhost":
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = "smtp.gmail.com"
     EMAIL_PORT = 587
     EMAIL_HOST_USER = os.environ.get("EMAIL_ID")
