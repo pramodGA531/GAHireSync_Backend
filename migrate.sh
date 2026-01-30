37d3086262aea51ae3488edd3aabb974b66f1e89
Sai Venkata Sirisha Motupalli <motupallisnmsdurgapramod531@gmail.com>
2026-01-30 14:54:45 +0530

    migrate.sh modified

--- migrate.sh
+++ migrate.sh
@@ -1,9 +1,10 @@
-export DJANGO_SUPERUSER_USERNAME=admin && \
-export DJANGO_SUPERUSER_EMAIL=admin@example.com && \
-export DJANGO_SUPERUSER_PASSWORD=admin && \
-python manage.py migrate && \
-python manage.py createsuperuser --noinput \
-  --username "$DJANGO_SUPERUSER_USERNAME" \
-  --email "$DJANGO_SUPERUSER_EMAIL" && \
+# export DJANGO_SUPERUSER_USERNAME=admin && \
+# export DJANGO_SUPERUSER_EMAIL=admin@example.com && \
+# export DJANGO_SUPERUSER_PASSWORD=admin && \
+python manage.py migrate
+#  && \
+# python manage.py createsuperuser --noinput \
+  # --username "$DJANGO_SUPERUSER_USERNAME" \
+  # --email "$DJANGO_SUPERUSER_EMAIL" && \
 gunicorn RTMAS_BACKEND.wsgi:application --workers 1 --threads 2
 # gunicorn RTMAS_BACKEND.wsgi:application --bind 0.0.0.0:$PORT
